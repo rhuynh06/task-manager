3c9fa05a52565729c197de0a72383755b29fdd26
ryan huynh <ryanhuynh200604@gmail.com>
2025-06-24 15:20:59 -0700

    added docs for demployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '',
   plugins: [react()],
+  base: "/task-manager/",
   build: {
-    outDir: 'build'
+    outDir: "./docs",
+    emptyOutDir: true
   }
 })
